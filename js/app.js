a844ba369418b3fdef8fa51cf7aa232dcee65eab
BogneStanley <bogne.stanley@gmail.com>
2020-10-12 23:00:21 +0100

    purification des fichiers et creation de la page apropos version-1

--- js/app.js
+++ js/app.js
@@ -1,18 +1,6 @@
-Aos.init()
-window.onload = function (param) { 
-    let nav = document.querySelector(".navbar");
-    let slog = document.querySelector(".slog")
-    let Header = document.querySelector("header")
-    navHeight = nav.clientHeight
+AOS.init()
 
-    slog.style.position = "relative";
-    slog.style.top = navHeight*1.5 + "px";
 
-    document.addEventListener("scroll",function (e) {  
-        console.log(Header.getBoundingClientRect().bottom <= nav.getBoundingClientRect().bottom);
-    })
-
-    
-    
-
+window.onload = function (param) {
+      
 }
